7fc4c7bd773abb06720233d8f42f0fbc0dd09765
tm-sukehiro <k.sukehiro@tcmobile.jp>
2016-08-22 15:50:20 +0900

    add test

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -8,9 +8,11 @@ interface Props {
 
 export default class Counter extends React.Component<Props, {}> {
   render() {
+    const loading = (this.props.value.loadingCount === 0) ? <p></p> : <p>loading</p>;
     return (
       <div>
-        <p>Clicked: {this.props.value.num} times</p>
+        {loading}
+        <p>score: {this.props.value.num}</p>
         <button onClick={() => this.props.actions.increment(3)}>Increment 3</button>
         <button onClick={() => this.props.actions.decrement(2)}>Decrement 2</button>
         <button onClick={() => this.props.actions.fetchAmount()}>async bonus 100</button>
